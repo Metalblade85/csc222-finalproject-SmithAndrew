fd292ab30c98e8435cd06a15e8308bea74a48334
Metalblade85 <196824798+Metalblade85@users.noreply.github.com>
2025-11-23 00:15:10 -0500

    Finished Main to display vehicles

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Make Car object
+        Car sedan= new Car("Toyota", "Corolla", 2022, 15000, 5);
 
+        //Make Truck Object
+        Truck pickup = new Truck("Chevrolet", "Silverado", 2020, 30000, 2000);
 
+        //Print test
+        System.out.println("Car rental test:");
+        sedan.displayDetails();
+        pickup.displayDetails();
 
     }
 }
