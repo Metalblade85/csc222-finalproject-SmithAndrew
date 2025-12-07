24222555d0c7de253e7ef90248291c020e8b4eff
Metalblade85 <196824798+Metalblade85@users.noreply.github.com>
2025-12-07 17:38:42 -0500

    Added addItem helper method

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,6 +7,7 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         //Menu
         String choice;
         do {
@@ -28,5 +30,59 @@ public class Main {
         }while (!choice.equals("3"));
 
         scanner.close();
+
+    }//printHeader menu
+    private static void printHeader() {
+        System.out.println("\nBlue Ridge Car Rental system");
+        System.out.println("1) Add a Vehicle");
+        System.out.println("2) List Inventory");
+        System.out.println("3) Exit");
+        System.out.print("Enter choice: ");
     }
+
+    //Adding a vehicle
+    private static void addItem() {
+        System.out.println("\nAdd new vehicle");
+        System.out.println("Adding a Car or Truck? (Type C or T");
+        String type = scanner.nextLine().trim().toUpperCase();
+
+        System.out.println("Enter Make: ");
+        String make =scanner.nextLine();
+
+        System.out.println("Enter Model: ");
+        String model =scanner.nextLine();
+
+        System.out.println("Enter Year: ");
+        int year = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter Mileage: ");
+        int mileage = Integer.parseInt(scanner.nextLine());
+
+        //Car or Truck logic
+        if (type.equals("C")) {
+            System.out.println("Enter maximum passengers: ");
+            int passengers = Integer.parseInt(scanner.nextLine());
+
+            //Create car object and add to list
+            Car newCar = new Car(make, model, year, mileage, passengers);
+            inventory.add(newCar);
+            System.out.println(">> Car added successfully!");
+
+        } else if (type.equals("T")) {
+            System.out.print("Enter payload capacity (lbs): ");
+            int payload = Integer.parseInt(scanner.nextLine());
+
+            //Create truck object and add to list
+            Truck newTruck = new Truck(make, model, year, mileage, payload);
+            inventory.add(newTruck);
+            System.out.println(">> Truck added successfully!");
+
+        } else {
+            System.out.println("Invalid selection");
+        }
+
+    }
+
+
+
 }
