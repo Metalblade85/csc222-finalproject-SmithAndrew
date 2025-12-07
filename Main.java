209e0f1986a031eae3d0953f7def43df78244fb3
Metalblade85 <196824798+Metalblade85@users.noreply.github.com>
2025-12-07 17:07:17 -0500

    Added a menu

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static ArrayList<Vehicle> inventory = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        //Make Car object
-        Car sedan= new Car("Toyota", "Corolla", 2022, 15000, 5);
+        //Menu
+        String choice;
+        do {
+            printHeader();
+            choice = scanner.nextLine();
 
-        //Make Truck Object
-        Truck pickup = new Truck("Chevrolet", "Silverado", 2020, 30000, 2000);
+            switch (choice) {
+                case "1":
+                    addItem();
+                    break;
+                case "2":
+                    listItems();
+                    break;
+                case "3":
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid option. Try again");
+            }
+        }while (!choice.equals("3"));
 
-        //Print test
-        System.out.println("Car rental test:");
-        sedan.displayDetails();
-        pickup.displayDetails();
-
+        scanner.close();
     }
 }
